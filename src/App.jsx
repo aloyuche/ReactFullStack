7d2e60ccefe16ead35b2783fa15e37273855cb3a
aloyuche <aloynwa@gmail.com>
2022-10-22 22:10:36 +0100

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "bootstrap/dist/css/boostrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 
 export default class App extends Component {
