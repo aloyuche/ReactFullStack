f5719b862c79e378c74a7c76ccfc5a6c2a2a5ee0
aloyuche <aloynwa@gmail.com>
2022-10-25 16:52:56 +0100

    ed

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -21,50 +21,60 @@ const SignIn = () => {
     setSuccess(true);
   };
   return (
-    <div>
-      <section>
-        <p
-          ref={errRef}
-          className={errMsg ? "errmsg" : "offscreen"}
-          aria-live="assertive"
-        >
-          {errMsg}
-        </p>
-      </section>
-      <form action="" className="form-control" onSubmit={handleSubmit}>
-        <h1>SIGN IN</h1>
+    <>
+      {success ? (
+        <section>
+          <h1>You Logged in</h1> <br />
+          <p>
+            <a href="#">Go to HomePage</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
 
-        <label htmlFor="username">UserName</label>
-        <input
-          type="text"
-          onChange={(e) => setUser(e.target.value)}
-          id="username"
-          ref={userRef}
-          autoComplete="off"
-          className="form-group"
-          value={user}
-          required
-        />
-        <br />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          onChange={(e) => setPwd(e.target.value)}
-          id="password"
-          className="form-group"
-          value={pwd}
-          required
-        />
-        <br />
-        <button className="btn btn-danger">Sign Up</button>
-        <p>
-          New User? <br />
-          <span className="line">
-            <a href="/register">Sign Up</a>
-          </span>
-        </p>
-      </form>
-    </div>
+          <form action="" className="form-control" onSubmit={handleSubmit}>
+            <h1>SIGN IN</h1>
+
+            <label htmlFor="username">UserName</label>
+            <input
+              type="text"
+              onChange={(e) => setUser(e.target.value)}
+              id="username"
+              ref={userRef}
+              autoComplete="off"
+              className="form-group"
+              value={user}
+              required
+            />
+            <br />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              onChange={(e) => setPwd(e.target.value)}
+              id="password"
+              className="form-group"
+              value={pwd}
+              required
+            />
+            <br />
+            <button className="btn btn-danger">Sign Up</button>
+            <p>
+              New User? <br />
+              <span className="line">
+                <a href="/register">Sign Up</a>
+              </span>
+            </p>
+          </form>
+        </section>
+      )}
+    </>
   );
 };
 
